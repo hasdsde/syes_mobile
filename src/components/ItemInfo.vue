e0c8b460a1ad512d116715c28d691537b955a153
hasdsde <1259305706@qq.com>
2022-09-07 18:10:16 +0800

    评论需要大改

--- src/components/ItemInfo.vue
+++ src/components/ItemInfo.vue
@@ -38,24 +38,12 @@
       <q-card class="my-card q-pa-sm no-shadow q-mt-md q-ml-none" v-ripple.early>
         <p class="q-pt-xs q-pl-xs  no-margin text-weight-bold">出价</p>
         <div class="q-pl-md q-pr-md q-pt-sm q-pb-sm">
-          暂无用户出价，说出你的价格!？
-          <q-chip clickable color="white" text-color="black">
+          <span v-if="Auction.length===0" class="text-grey-6"> 暂无用户出价，说出你的价格!？</span>
+          <q-chip clickable color="white" text-color="black" v-for="item in Auction" :key="item.id">
             <q-avatar>
-              <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+              <img :src="item.avatar">
             </q-avatar>
-            田所浩二 出价：<span class="text-orange">￥200</span>
-          </q-chip>
-          <q-chip clickable color="white" text-color="black">
-            <q-avatar>
-              <img src="https://cdn.quasar.dev/img/boy-avatar.png">
-            </q-avatar>
-            田所浩二 出价：<span class="text-orange">￥200</span>
-          </q-chip>
-          <q-chip clickable color="white" text-color="black">
-            <q-avatar>
-              <img src="https://cdn.quasar.dev/img/boy-avatar.png">
-            </q-avatar>
-            田所浩二 出价：<span class="text-orange">￥200</span>
+            {{ item.username }} 出价：<span class="text-orange">￥{{ item.price }}</span>
           </q-chip>
         </div>
       </q-card>
@@ -63,19 +51,24 @@
       <q-card class="my-card q-pa-sm no-shadow q-mt-md q-ml-none">
         <p class="q-pt-xs q-pl-xs  no-margin text-weight-bold">评论</p>
         <q-list>
-          <q-expansion-item expand-icon-class="hidden">
+          <q-expansion-item expand-icon-class="hidden" v-for="item in FComment" :key="item.id"
+                            @click="handleEComment(item.id)">
             <template v-slot:header>
               <q-item-section avatar class="vertical-top">
-                <q-avatar icon="bluetooth" color="primary" text-color="white"/>
+                <q-avatar text-color="white">
+                  <img :src="item.avatar" alt="">
+                </q-avatar>
               </q-item-section>
               <q-item-section>
                 <div class="no-margin">
-                  <span class="text-primary">刘华强</span>
-                  <span class="text-caption text-grey-7">20 计算机</span>
-                  <span class="no-margin text-caption text-grey-7 float-right">8月8日</span>
+                  <span class="text-primary">{{ item.nickname }}</span>
+                  <span class="text-caption text-grey-7">{{ item.userid }} 计算机</span>
+                  <span class="no-margin text-caption text-grey-7 float-right">{{
+                      item.createtime
+                    }}</span>
                 </div>
-                <p class="no-margin">你这瓜保熟吗？</p>
-                <span class="no-margin text-caption text-grey-7" v-ripple.early>查看3条回复</span>
+                <p class="no-margin">{{ item.content }}</p>
+                <span class="no-margin text-caption text-grey-7" v-ripple.early v-if="item.counts>0">点击查看回复</span>
               </q-item-section>
             </template>
             <q-card>
@@ -137,45 +130,6 @@
             </q-card>
           </q-expansion-item>
 
-          <q-expansion-item expand-icon-class="hidden">
-            <template v-slot:header>
-              <q-item-section avatar class="vertical-top">
-                <q-avatar icon="bluetooth" color="primary" text-color="white"/>
-              </q-item-section>
-              <q-item-section>
-                <div class="no-margin">
-                  <span class="text-primary">奥特曼大王</span>
-                  <span class="text-caption text-grey-7">20 计算机</span>
-                  <span class="no-margin text-caption text-grey-7 float-right">8月8日</span>
-                </div>
-                <p class="no-margin">我是学生，50包邮给我</p>
-                <span class="no-margin text-caption text-grey-7" v-ripple.early>查看1条回复</span>
-              </q-item-section>
-            </template>
-            <q-card>
-              <q-card-section>
-                <q-list>
-                  <q-item clickable v-ripple class="q-pr-none" v-ripple.early>
-                    <q-item-section avatar>
-                      <q-avatar>
-                        <img src="https://cdn.quasar.dev/img/avatar2.jpg">
-                      </q-avatar>
-                    </q-item-section>
-                    <q-item-section>
-                      <q-item-label lines="1">
-                        <span class="text-primary">老板 </span>回复：
-                        <span class="text-primary">奥特曼大王</span>
-                        <span class="no-margin text-caption text-grey-7 float-right">8月8日</span>
-                      </q-item-label>
-                      <q-item-label>
-                        你是畜生也不行
-                      </q-item-label>
-                    </q-item-section>
-                  </q-item>
-                </q-list>
-              </q-card-section>
-            </q-card>
-          </q-expansion-item>
         </q-list>
       </q-card>
       <!--第五部分 空白站位-->
@@ -200,6 +154,7 @@ let itemid = ref()
 let itemDetail = ref({})
 let imgDetail = ref([])
 let Auction = ref([])
+let FComment = ref([])
 
 //监测网址操作，返回物品id
 watch(() => $router.currentRoute.value.query, (newValue, oldValue) => {
@@ -212,6 +167,7 @@ watch(() => $router.currentRoute.value.query, (newValue, oldValue) => {
 loadItemInfo()
 loadImginfo()
 loadAuction()
+loadFComment()
 
 //获取物品详细信息
 function loadItemInfo() {
@@ -234,8 +190,22 @@ function loadImginfo() {
 function loadAuction() {
   api.get('/auction/queryItem/' + itemid.value).then(res => {
     Auction.value = res.data
-    console.log(res.data)
   })
+}
+
+//获取全部父级评论
+function loadFComment() {
+  api.get('/comment/f?itemid=' + itemid.value).then(res => {
+    for (let i = 0; i < res.data.length; i++) {
+      res.data[i].createtime = res.data[i].createtime.slice(5, 10)
+    }
+    FComment.value = res.data
+  })
+}
+
+//获取子集评论
+function handleEComment(value: any) {
+  console.log(value)
 }
 
 //下拉刷新

a6f839500ade18ae43956484076621932dc39b39
hasdsde <1259305706@qq.com>
2022-10-17 13:52:17 +0800

    去掉了手机版首页加载图标，那个问题太大了

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -24,9 +24,9 @@
       <!--  末尾自动加载  -->
       <q-infinite-scroll @load="onLoad" :offset="100" debounce="1000">
         <!--  列出的物品  -->
-        <div class="q-pa-md   q-gutter-md">
+        <div>
           <!--重写布局-->
-          <div class="bg-grey-2 fit row wrap justify-center items-start content-start no-margin">
+          <div class="bg-grey-2 fit row wrap justify-center items-start content-start no-margin mobile-hide">
             <q-card v-ripple.early class="myCardPC justify-around q-ma-md " v-for="item in itemInfoAll"
                     :key="item.id"
                     @click="handleClick(item.id)">
@@ -55,69 +55,68 @@
             </q-card>
           </div>
           <!--左侧列和右侧列-->
-          <div>
-            <!--          &lt;!&ndash;左侧列&ndash;&gt;-->
-            <!--          <div class="column bg-grey-2" style="padding: 0 ;margin: auto;width: 50% ;position: absolute;">-->
-            <!--            <q-card v-ripple.early class="my-card justify-around" v-for="item in itemInfo1" :key="item.id"-->
-            <!--                    @click="handleClick(item.id)">-->
-            <!--              <q-img :src="item.url"/>-->
-            <!--              <q-card-section style="padding: 0.1rem 0.5rem;max-height: 4rem">-->
-            <!--                <div class="row no-wrap items-center" style="padding: 0">-->
-            <!--                  <div class="col text-body1  text-weight-bold ellipsis-2-lines"-->
-            <!--                       style="font-size: 1rem;padding-top: 0.5rem;">-->
-            <!--                    {{ item.title }}...-->
-            <!--                  </div>-->
-            <!--                </div>-->
-            <!--              </q-card-section>-->
-            <!--              <q-separator/>-->
-            <!--              <q-card-section class=" q-pt-none-->
-            <!--            " style="padding-top: 10px">-->
-            <!--                <div class="text-subtitle1 ">-->
-            <!--                  ￥{{ item.price }}-->
-            <!--                  <span class="text-subtitle2 text-caption">{{ item.collectionnum }}人想要</span>-->
-            <!--                </div>-->
-            <!--                <div class="text-caption text-grey">-->
-            <!--                  {{ item.nickname }}-->
-            <!--                  <br>-->
-            <!--                  {{ item.grade }}计算机 发布于{{ item.createtime }}-->
-            <!--                </div>-->
-            <!--              </q-card-section>-->
-            <!--            </q-card>-->
-            <!--          </div>-->
-            <!--          &lt;!&ndash;右侧列&ndash;&gt;-->
-            <!--          <div class="column bg-grey-2" style="padding: 0 ;margin: auto;width: 50%;position: absolute;left: 50%">-->
-            <!--            <q-card v-ripple.early class="my-card justify-around" v-for="item in itemInfo2" :key="item.id"-->
-            <!--                    @click="handleClick(item.id)">-->
-            <!--              <q-img :src="item.url"/>-->
-            <!--              <q-card-section style="padding: 0.1rem 0.5rem;max-height: 4rem">-->
-            <!--                <div class="row no-wrap items-center" style="padding: 0">-->
-            <!--                  <div class="col text-body1  text-weight-bold ellipsis-2-lines"-->
-            <!--                       style="font-size: 1rem;padding-top: 0.5rem;">-->
-            <!--                    {{ item.title }}...-->
-            <!--                  </div>-->
-            <!--                </div>-->
-            <!--              </q-card-section>-->
-            <!--              <q-separator/>-->
-            <!--              <q-card-section class=" q-pt-none-->
-            <!--            " style="padding-top: 10px">-->
-            <!--                <div class="text-subtitle1 ">-->
-            <!--                  ￥{{ item.price }}-->
-            <!--                  <span class="text-subtitle2 text-caption">{{ item.collectionnum }}人想要</span>-->
-            <!--                </div>-->
-            <!--                <div class="text-caption text-grey">-->
-            <!--                  {{ item.nickname }}-->
-            <!--                  <br>-->
-            <!--                  {{ item.grade }}计算机 发布于{{ item.createtime }}-->
-            <!--                </div>-->
-            <!--              </q-card-section>-->
-            <!--            </q-card>-->
-
-            <!--          </div>-->
+          <div class="mobile-only">
+            <!--左侧列-->
+            <div class="column bg-grey-2" style="padding: 0 ;margin: auto;width: 50% ;position: absolute;">
+              <q-card v-ripple.early class="my-card justify-around" v-for="item in itemInfo1" :key="item.id"
+                      @click="handleClick(item.id)">
+                <q-img :src="item.url"/>
+                <q-card-section style="padding: 0.1rem 0.5rem;max-height: 4rem">
+                  <div class="row no-wrap items-center" style="padding: 0">
+                    <div class="col text-body1  text-weight-bold ellipsis-2-lines"
+                         style="font-size: 1rem;padding-top: 0.5rem;">
+                      {{ item.title }}...
+                    </div>
+                  </div>
+                </q-card-section>
+                <q-separator/>
+                <q-card-section class=" q-pt-none
+                        " style="padding-top: 10px">
+                  <div class="text-subtitle1 ">
+                    ￥{{ item.price }}
+                    <span class="text-subtitle2 text-caption">{{ item.collectionnum }}人想要</span>
+                  </div>
+                  <div class="text-caption text-grey">
+                    {{ item.nickname }}
+                    <br>
+                    {{ item.grade }}计算机 发布于{{ item.createtime }}
+                  </div>
+                </q-card-section>
+              </q-card>
+            </div>
+            <!--右侧列-->
+            <div class="column bg-grey-2" style="padding: 0 ;margin: auto;width: 50%;position: absolute;left: 50%">
+              <q-card v-ripple.early class="my-card justify-around" v-for="item in itemInfo2" :key="item.id"
+                      @click="handleClick(item.id)">
+                <q-img :src="item.url"/>
+                <q-card-section style="padding: 0.1rem 0.5rem;max-height: 4rem">
+                  <div class="row no-wrap items-center" style="padding: 0">
+                    <div class="col text-body1  text-weight-bold ellipsis-2-lines"
+                         style="font-size: 1rem;padding-top: 0.5rem;">
+                      {{ item.title }}...
+                    </div>
+                  </div>
+                </q-card-section>
+                <q-separator/>
+                <q-card-section class=" q-pt-none
+                        " style="padding-top: 10px">
+                  <div class="text-subtitle1 ">
+                    ￥{{ item.price }}
+                    <span class="text-subtitle2 text-caption">{{ item.collectionnum }}人想要</span>
+                  </div>
+                  <div class="text-caption text-grey">
+                    {{ item.nickname }}
+                    <br>
+                    {{ item.grade }}计算机 发布于{{ item.createtime }}
+                  </div>
+                </q-card-section>
+              </q-card>
+              <!--              <q-spinner-dots style="left: 50%;position: absolute" color="primary" size="40px"/>-->
+            </div>
           </div>
-
         </div>
         <!--加载图表-->
-        <q-spinner-dots style="left: 50%;position: absolute" color="primary" size="40px"/>
+        <q-spinner-dots class="mobile-hide" style="left: 50%;position: absolute" color="primary" size="40px"/>
       </q-infinite-scroll>
 
     </q-pull-to-refresh>
@@ -177,14 +176,15 @@ function loadPage() {
       res.data[i].createtime = res.data[i].createtime.slice(5, 10)
       //@ts-ignore
       itemInfoAll.value.push(res.data[i])
-      // @ts-ignore
-      // if (bool) {
-      //   //@ts-ignore
-      //   itemInfo1.value.push(res.data[i])
-      // } else {
-      //   //@ts-ignore
-      //   itemInfo2.value.push(res.data[i])
-      // }
+      // @ts
+      //   -ignore
+      if (bool) {
+        //@ts-ignore
+        itemInfo1.value.push(res.data[i])
+      } else {
+        //@ts-ignore
+        itemInfo2.value.push(res.data[i])
+      }
       bool = !bool
     }
     console.log(itemInfoAll.value)
